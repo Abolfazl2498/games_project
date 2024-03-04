1aa576dbdecb2d02a3d515facf360a27fd56880e
Abolfazl2498 <abolfazl.rostami9099@gmail.com>
2024-03-04 12:03:56 +0330

    fix(Displayin critic score)

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+        let color = score > 90 ? "green" : score > 75 ? "yellow" : "";
+
+  return <Badge colorScheme={color}>{score}</Badge>;
+};
+
+export default CriticScore;
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-export interface Platform{
-    id:number
-    name:string
-    slug:string
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
   id: number;
   name: string;
-  background_image:string
-  parent_platforms:{platform:Platform}[]
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic:number
 }
 interface FetchGamesResponse {
   count: number;
   results: Game[];
 }
 
-const useGame =()=>{
-      const [games, setGames] = useState<Game[]>([]);
-      const [error, setError] = useState("");
+const useGame = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
 
-      useEffect(() => {
-        apiClient
-          .get<FetchGamesResponse>("/games")
-          .then((res) => setGames(res.data.results))
-          .catch((err) => setError(err.message));
-      });
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => setError(err.message));
+  });
 
-      return {games ,error}
+  return { games, error };
+};
 
-}
-
-export default useGame
+export default useGame;
