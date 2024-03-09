3ea422963e24bf5e3fc613f030d3e0a809358b79
Abolfazl2498 <abolfazl.rostami9099@gmail.com>
2024-03-09 08:26:36 +0330

    fix(the issue with some games not having platforms)

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -2,36 +2,38 @@ import {
   FaWindows,
   FaPlaystation,
   FaXbox,
-  FaLinux,
   FaApple,
+  FaLinux,
   FaAndroid,
 } from "react-icons/fa";
-import { MdPhoneIphone } from "react-icons/md";
-import { SiNatsdotio } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-import { HStack, Icon, Text } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGame";
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
 import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
-const PlatformIconList = ({ platforms }: Props) => {
-  const iconMap: { [key: string]: IconType } = {
+
+const PlatformIconList = ({ platforms = [] }: Props) => {
+  const iconMap: { [key: string]: IconType } = { 
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
-    android: FaAndroid,
-    nintendo: SiNatsdotio,
+    nintendo: SiNintendo,
     mac: FaApple,
-    linux: FaLinux,
+    linux: FaLinux, 
+    android: FaAndroid,
     ios: MdPhoneIphone,
-    wec: BsGlobe,
-  };
+    web: BsGlobe
+  }
+
   return (
-    <HStack marginY={1}>
+    <HStack marginY={1}> 
       {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'/>
       ))}
     </HStack>
   );
